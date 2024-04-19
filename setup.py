1d2dc068bdd6c6a6e8fc9ffe0385366aca415614
Francisco Carmona <jcarmfran@gmail.com>
2024-04-19 11:07:59 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name = "Generative AI Project",
+    version = "0.0.0",
+    author = "Cisco",
+    author_email= "jcarmfran@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
